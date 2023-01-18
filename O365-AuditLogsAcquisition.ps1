f55ca273973f59fcfd42d7ecc6dc043155791425
f1r3walled <f1r3walled@users.noreply.github.com>
2023-01-18 15:39:27 -0300

    Add files via upload

---
+++ O365-AuditLogsAcquisition.ps1
@@ -0,0 +1,17 @@
+    $UserCredential = Get-Credential
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+    Import-PSSession $Session -DisableNameChecking
+    $OutputFile = "C:\Users\Public\Documents\YourFileName.csv"
+    $Today = Get-Date -Date (Get-Date -Format “yyyy-MM-dd”)
+    $intDays = 30
+    For ($i=0; $i -le $intDays; $i++){
+      For ($j=23; $j -ge 0; $j--){
+        $StartDate = ($Today.AddDays(-$i)).AddHours($j)
+        $EndDate = ($Today.AddDays(-$i)).AddHours($j + 1)
+        $Audit = Search-UnifiedAuditLog -StartDate $StartDate -EndDate $EndDate -ResultSize 5000
+        $ConvertAudit = $Audit | Select-Object -ExpandProperty AuditData | ConvertFrom-Json
+        $ConvertAudit | Select-Object CreationTime,UserId,Operation,Workload,ObjectID,SiteUrl,SourceFileName,ClientIP,UserAgent | Export-Csv $OutputFile -NoTypeInformation -Append
+        Write-Host $StartDate `t $Audit.Count
+      }
+    }
+
